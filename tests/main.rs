bc76a239937d7717c90fae2031f0e7b83ba61a0f
bouzuya <m@bouzuya.net>
2025-12-20 07:55:34 +0900

    Add tests/

---
+++ tests/main.rs
@@ -0,0 +1,21 @@
+use predicates::prelude::PredicateBooleanExt;
+
+#[test]
+fn test_no_subcommand() {
+    assert_cmd::Command::new("cargo")
+        .args(["run"])
+        .assert()
+        .failure()
+        .stderr(
+            predicates::str::contains("Usage: fubako <COMMAND>")
+                .and(predicates::str::contains("Commands:"))
+                .and(predicates::str::contains("  edit"))
+                .and(predicates::str::contains("  get"))
+                .and(predicates::str::contains("  image"))
+                .and(predicates::str::contains("  new"))
+                .and(predicates::str::contains("  serve"))
+                .and(predicates::str::contains("  help"))
+                .and(predicates::str::contains("Options:"))
+                .and(predicates::str::contains("  -h, --help")),
+        );
+}

43b52b9bba0aa1d11f6ac9bb19172075dbc52cd2
bouzuya <m@bouzuya.net>
2025-12-24 06:46:29 +0900

    Add title url copy button

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,34 +1,50 @@
-function addClipboardCopyButton() {
+function addPageIdCopyButton() {
     const li = document.querySelector(".breadcrumb_section > ol > li:nth-child(2)");
-    if (li !== null) {
-        const pageId = li.textContent.trim();
-        if (pageId === "pages" || pageId === "titles") return;
-        const button = document.createElement("button");
-        button.type = "button";
-        button.className = "clipboard_copy_button";
-        button.textContent = "📋";
-        button.addEventListener("click", async () => {
-            button.disabled = true;
-            try {
-                await navigator.clipboard.writeText(pageId);
-                button.textContent = "✔";
-                await new Promise((resolve) => {
-                    setTimeout(() => {
-                        button.textContent = "📋";
-                        resolve();
-                    }, 1200);
-                });
-            } finally {
-                button.disabled = false;
-            }
-        });
-        li.appendChild(button);
-    }
+    if (li === null) return;
+    const pageId = li.textContent.trim();
+    if (pageId === "pages" || pageId === "titles") return;
+    const button = createClipboardCopyButton(pageId);
+    li.appendChild(button);
+}
 
+function addTitleUrlCopyButton() {
+    const p = document.querySelector(".page_title_section > p");
+    if (p === null) return;
+    const a = p.querySelector("a");
+    if (a === null) return;
+    const href = a.getAttribute("href");
+    if (href === null) return;
+    const button = createClipboardCopyButton(href);
+    p.appendChild(button);
+}
+
+// string -> HTMLButtonElement
+function createClipboardCopyButton(text) {
+    const button = document.createElement("button");
+    button.type = "button";
+    button.className = "clipboard_copy_button";
+    button.textContent = "📋";
+    button.addEventListener("click", async () => {
+        button.disabled = true;
+        try {
+            await navigator.clipboard.writeText(text);
+            button.textContent = "✔";
+            await new Promise((resolve) => {
+                setTimeout(() => {
+                    button.textContent = "📋";
+                    resolve();
+                }, 1200);
+            });
+        } finally {
+            button.disabled = false;
+        }
+    });
+    return button;
 }
 
 function main() {
-    addClipboardCopyButton();
+    addPageIdCopyButton();
+    addTitleUrlCopyButton();
 }
 
 main();
